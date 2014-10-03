a431ff7c98666c9673f220588f1dd60fe0b8b823
SeedPaths-User <lessdismissive@email.com>
2014-10-03 16:57:06 -0600

    Publishing

---
+++ Week6CodeChallenge/Week6CodeChallenge/Controllers/AboutController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week6CodeChallenge.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Who()
+        {
+            return PartialView();
+        }
+
+        public ActionResult What()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Why()
+        {
+            return PartialView();
+        }
+
+        public ActionResult How()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Where()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Week6CodeChallenge/Week6CodeChallenge/Scripts/main.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    //SUPERIOR AJAX GET 
+    //Use 2nd select for applying this function to any matching elements that appear on the page AT ANY TIME
+    $('body').on('click', '.ajax-get', function () {
+        var urlRequest = $(this).data('url');
+        //Make the AJAX request
+        $.get(urlRequest, function (data) {
+            $('#content').html(data);
+        });
+    });
+    //AJAX GET END
+
+    //AJAX POST for Contact Form START
+    $('#contactForm').on('submit', function (event) {
+        //Prevent default form behavior (Doesn't allow it to be submitted)
+        event.preventDefault();
+        //See if form is valid
+        if ($(this).valid()) {
+            //AJAX POST
+            var urlToPostTo = $(this).attr('action');
+            //Serialize to convert the form fields to a string we can pass into our $.post() function
+            var dataToSend = $(this).serialize();
+            $.post(urlToPostTo, dataToSend, function (data) {
+                //Update the #container elements with the new HTML returned in the "data" param
+                $('#container').html(data);
+            });
+        }
+    });
+    //AJAX POST for Contact Form END
+});
